bc588fccdbde0189b36653c29d9630505ae7022a
gallojunior <gallojunior@gmail.com>
2024-04-23 15:10:59 -0300

    Avaliação

--- ConectaCafe/wwwroot/assets/js/site.js
+++ ConectaCafe/wwwroot/assets/js/site.js
@@ -1,5 +1,16 @@
 let elements = document.querySelectorAll('input[type="text"], select, textarea');
 elements.forEach((elem, index, array) => {
+    let placeholder = elem.placeholder;
+    if (placeholder != "") {
+        elem.placeholder = "";
+        elem.onfocus = () => {
+            elem.placeholder = placeholder
+        };
+        elem.onblur = () => {
+            elem.placeholder = "";
+        }
+    }
+
     if (elem.value != "") {
         elem.parentElement.classList.add('is-filled');
     }
